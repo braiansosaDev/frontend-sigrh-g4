a4fbb15a4f7118cd3b6d54da1cddc6d1af15be11
Gonzalo Faccini <gonzalo.faccini@gmail.com>
2025-05-10 16:33:47 -0300

    Validaciones para mails repetidos en convocatoria y cantidad máxima de postulaciones

--- src/components/jobs/postulation.js
+++ src/components/jobs/postulation.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import config from "@/config";
 import Cookies from "js-cookie";
 import axios from "axios";
@@ -17,6 +17,7 @@ export default function PostulationModal({ onClose, jobTitle, jobId }) {
   const [language, setLanguage] = useState(""); // Idioma del CV
   const [countries, setCountries] = useState([]); // Lista de países
   const [states, setStates] = useState([]); // Lista de provincias
+  const [postulations, setPostulations] = useState([]); // Lista de postulaciones
   const [cvFile, setCvFile] = useState(null); // Almacena el archivo del CV
 
   const fetchCountries = async () => {
@@ -41,10 +42,32 @@ export default function PostulationModal({ onClose, jobTitle, jobId }) {
     }
   };
 
+  const fetchJobOpportunityPostulations = async () => {
+    try {
+      const res = await axios.get(`${config.API_URL}/postulations/`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (res.status !== 200) throw new Error("Error al obtener postulaciones");
+
+      setPostulations(
+        res.data.filter(
+          (postulation) => postulation.job_opportunity_id === jobId
+        )
+      );
+    } catch (e) {
+      alert("No se pudieron obtener las postulaciones");
+    }
+  };
+
   useEffect(() => {
     fetchCountries();
     fetchStates();
+    fetchJobOpportunityPostulations();
   }, []);
+
+  useEffect(() => {
+    fetchJobOpportunityPostulations();
+  }, [step]);
 
   const handleCountryChange = (e) => {
     setCountryId(e.target.value);
@@ -66,6 +89,18 @@ export default function PostulationModal({ onClose, jobTitle, jobId }) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       alert("Por favor, ingresa un email válido.");
+      return false;
+    }
+
+    // Validar que el email no esté ya registrado en la misma oferta
+    const emailExists = postulations.some(
+      (postulation) =>
+        postulation.email === email && postulation.job_opportunity_id === jobId
+    );
+    if (emailExists) {
+      alert(
+        "Ya existe una postulación con este email para esta oferta de trabajo."
+      );
       return false;
     }
 
@@ -129,10 +164,19 @@ export default function PostulationModal({ onClose, jobTitle, jobId }) {
       return false;
     }
 
+    // Validar que haya mil cvs o menos
+    const cvsCount = postulations.length;
+    if (cvsCount >= 1000) {
+      alert(
+        "Lo sentimos, la capacidad máxima de postulaciones ha sido alcanzada."
+      );
+      return false;
+    }
+
     return true;
   };
 
-  const handleNext = async () => {
+  const handleNext = () => {
     if (step === 1) {
       if (!validateStep1()) {
         return;
